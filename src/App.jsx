a5e33d1567a0b8fc5b584f945aa6a9a8f9de1ca6
Hridhu P <hridhu24@gmail.com>
2025-10-01 17:32:40 +0530

    Added cards and button

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from "./Hero";
 import About from "./About";
+import Card from './Card';
+import LikeButton from './LikeButton';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showAbout, setShowAbout] = useState(true);
 
   return (
     <div>
       <Hero />
-      <About />
+      <br></br>
+      {/* Button to toggle About section */}
+      <button onClick={() => setShowAbout(!showAbout)}>
+        {showAbout ? "Hide About" : "Show About"}
+      </button>
+
+      {/* Conditionally render About */}
+      {showAbout && <About />}
+      <Card 
+        image="https://via.placeholder.com/200"
+        title="Crochet Doll"
+        description="Handmade amigurumi doll with cotton yarn"
+      />
+      <LikeButton />
+      <Card 
+        image="https://via.placeholder.com/200"
+        title="Crochet Bag"
+        description="Stylish crochet tote bag"
+      />
+      <LikeButton />
     </div>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+export default Card;
