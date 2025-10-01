46692280af96ac45bf76b99eadc25bf3ed41db48
Hridhu P <hridhu24@gmail.com>
2025-10-01 15:48:59 +0530

    Added Hero and About components with JSX basics

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  const hobby = "Crochet";
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>Hi, I'm Hridhu. I love {hobby} and creating cozy designs!</p>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero(){
+    return(
+        <section>
+            <h1>Welcome to Yarnarc Studios</h1>
+            <p>Showcasing my crochet creations</p>
+        </section>
+    )
+
+}
+
+export default Hero;
